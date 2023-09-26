bbac22fd8a5cf2c73872f47b425e929525be011f
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-09-26 23:23:53 +0600

    category list create

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,9 +1,18 @@
-
+import { useEffect, useState } from "react";
+import SingleCategory from "../SingleCategory/SingleCategory";
 
 const CategoryList = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(()=>{
+        fetch('categorylist.json')
+        .then(res=> res.json())
+        .then(data=> setCategories(data));
+    },[]);
     return (
-        <div>
-            <h2>categorylist</h2>
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-5 my-20 mx-5">
+             {
+                categories.map(category=> <SingleCategory key={category.id} category={category}></SingleCategory>)
+             }
         </div>
     );
 };
--- src/components/SingleCategory/SingleCategory.jsx
+++ src/components/SingleCategory/SingleCategory.jsx
@@ -1,11 +1,24 @@
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
-
-const SingleCategory = () => {
+const SingleCategory = ({ category }) => {
     return (
         <div>
-            <h2>singlecategory</h2>
+            <Link to={`/category-list/${category.id}`}>
+                <div className="card card-compact bg-base-100 shadow-xl my-5">
+                    <figure><img src={category.image} alt="img" className='w-96 h-60' /></figure>
+                    <div className="card-body">
+                        <p className='badge'>{category.category_type}</p>
+                        <h2 className="card-title">{category.title}</h2>
+                    </div>
+                </div>
+            </Link>
         </div>
     );
 };
+
+SingleCategory.propTypes = {
+    category: PropTypes.object.isRequired
+}
 
 export default SingleCategory;
