7857e7c7f52a4945c10a6710e371c60b0f80ddc7
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-09-27 01:21:48 +0600

    set dynamic color into cards  update

--- src/components/SingleDonation/SingleDonation.jsx
+++ src/components/SingleDonation/SingleDonation.jsx
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 const SingleDonation = ({singleDonate}) => {
     // console.log(singleDonate)
     return (
-        <div className="card lg:card-side bg-base-100 shadow-xl">
-            <figure><img src={singleDonate.image} alt="img" className='w-96 h-52'/></figure>
+        <div className="card lg:card-side bg-base-100 shadow-xl" style={{backgroundColor: singleDonate.color.card_bg}}>
+            <figure><img src={singleDonate.image} alt="img" className='w-72 h-full'/></figure>
             <div className="card-body">
-                <p className="">{singleDonate.category_type}</p>
-                <h2>{singleDonate.title}</h2>
-                <p>{singleDonate.price}</p>
+                <p className="badge rounded-none p-3 font-normal text-sm" style={{backgroundColor: singleDonate.color.category_bg, color: singleDonate.color.primary_color}}>{singleDonate.category_type}</p>
+                <h2 className='font-semibold text-2xl text-[#0B0B0B]'>{singleDonate.title}</h2>
+                <p className='font-semibold text-base' style={{color: singleDonate.color.primary_color}}>${singleDonate.price}.00</p>
                 <div className="card-actions">
-                    <Link to={`/category-list/${singleDonate.id}`}><button className="btn btn-primary">View Details</button></Link>
+                    <Link to={`/category-list/${singleDonate.id}`}><button className="btn btn-sm text-white" style={{backgroundColor: singleDonate.color.primary_color}}>View Details</button></Link>
                 </div>
             </div>
         </div>
