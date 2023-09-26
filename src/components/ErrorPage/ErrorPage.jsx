7b8edf8aadeca56f312da459261b1137975a314a
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-09-26 23:31:42 +0600

    create error page

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,21 @@
+import { Link, useRouteError } from "react-router-dom";
 
 
 const ErrorPage = () => {
-  return (
-    <div>
-      <h2>this is error page</h2>
-    </div>
-  );
+    const error = useRouteError();
+    return (
+        <div className="text-center">
+          <h2>Opps!!</h2>
+          <p>{error.statusText || error.message}</p>
+          {
+            error.status === 404 && <div>
+                <h3>Page Not Found</h3>
+                <p>Go back</p>
+                <Link to='/'><button className="btn">Home</button></Link>
+            </div>
+          }
+        </div> 
+    );
 };
 
 export default ErrorPage;
