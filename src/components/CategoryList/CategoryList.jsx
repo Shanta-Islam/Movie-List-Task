5aba16b76eb7c1351811f4a6c86059a4f836daf1
Shanta-Islam <shantaislamshorna68@gmail.com>
2023-09-27 19:05:08 +0600

    search functionality create

--- src/components/CategoryList/CategoryList.jsx
+++ src/components/CategoryList/CategoryList.jsx
@@ -1,26 +1,25 @@
-import { useEffect, useState } from "react";
 import SingleCategory from "../SingleCategory/SingleCategory";
 import PropTypes from 'prop-types'; 
 
-const CategoryList = ({handleSearchClick}) => {
-    
-    const [categories, setCategories] = useState([]);
-    useEffect(()=>{
-        fetch('categorylist.json')
-        .then(res=> res.json())
-        .then(data=> setCategories(data));
-    },[]);
+const CategoryList = ({categoriesList}) => {
+    // const [categories, setCategories] = useState([]);
+    // useEffect(()=>{
+    //     fetch('categorylist.json')
+    //     .then(res=> res.json())
+    //     .then(data=> setCategories(data));
+    // },[]);
+    // console.log(categoriesList)
     return (
         <div className="grid grid-cols-1 lg:grid-cols-4 gap-5 my-20 mx-5">
              {
-                categories.map(category=> <SingleCategory key={category.id} category={category} ></SingleCategory>)
+                categoriesList.map(category=> <SingleCategory key={category.id} category={category} ></SingleCategory>)
              }
              
         </div>
     );
 };
 CategoryList.propTypes = {
-    handleSearchClick: PropTypes.func,
+    categoriesList: PropTypes.array
     
 }
 export default CategoryList;
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,29 +3,39 @@ import Banner from "../Banner/Banner";
 import CategoryList from "../CategoryList/CategoryList";
 import { useState } from "react";
 
+
 const Home = () => {
     const categoriesItem = useLoaderData();
-    const [categories, setcategories] = useState(categoriesItem);
+    const [categoriesList, setcategoriesList] = useState(categoriesItem);
     const [searchVal, setSearchVal] = useState("");
-    const [searchResults, setsearchResults] = useState([]);
-    function handleSearchClick() {
-        if (searchVal === "") { 
-            setcategories(categoriesItem); 
-            return; 
+    // const [searchResults, setsearchResults] = useState([]);
+    const handleSearchClick = () => {
+        if (searchVal === "") {
+            setcategoriesList(categoriesItem); 
+            return;
         }
-        const filterBySearch = categoriesItem.filter((item) => {
-            if (item.category_type.toLowerCase().includes(searchVal.toLowerCase())) { 
-                return setsearchResults(item); 
-            }
-            // console.log(item.category_type.toLowerCase().includes(searchVal.toLowerCase()));
-            console.log(item)
-        })
-        setcategories(filterBySearch);
+        else {
+            const filterBySearch = categoriesItem.filter((item) => {
+                if (item.category_type.toLowerCase().includes(searchVal.toLowerCase())) {
+                    // console.log(item)
+                    return true;
+                }
+                
+                
+            })
+           
+            // console.log(searchResults)
+            setcategoriesList(filterBySearch);
+        }
+        
+        
     }
+
+    console.log(categoriesList)
     return (
         <div>
             <Banner handleSearchClick={handleSearchClick} setSearchVal={setSearchVal}></Banner>
-            <CategoryList handleSearchClick={handleSearchClick}></CategoryList>
+            <CategoryList handleSearchClick={handleSearchClick} categoriesList={categoriesList}></CategoryList>
         </div>
     );
 };
